daa718e25abbce572c15f8a1c98e8bc76fe817c4
Zoya <zoya.artsy@gmail.com>
2021-04-29 15:49:06 -0400

    Added about me

---
+++ FinalScript.js
@@ -0,0 +1,11 @@
+GalleryButton = document.getElementById("GalleryButton");
+
+GalleryButton.addEventListener("click", onButtonClick); 
+
+function onButtonClick() {  
+    myitem.style.color = "green";  // when the user clicks on the text in the firsttest element, it will become green
+    myitem.style.backgroundColor = "powderblue";
+    myitem.style.fontWeight = "bold";
+}
+
+
