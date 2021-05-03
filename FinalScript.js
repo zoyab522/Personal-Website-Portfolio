3fdbfbb2807661aa1cc56d95d9a3c1714e2bba26
Zoya <zoya.artsy@gmail.com>
2021-05-03 12:21:52 -0400

    navbar added
    Has working drop-down menus, added some blank files for the menus that I'll fill with content later :)

--- FinalScript.js
+++ FinalScript.js
@@ -2,10 +2,5 @@ GalleryButton = document.getElementById("GalleryButton");
 
 GalleryButton.addEventListener("click", onButtonClick); 
 
-function onButtonClick() {  
-    myitem.style.color = "green";  // when the user clicks on the text in the firsttest element, it will become green
-    myitem.style.backgroundColor = "powderblue";
-    myitem.style.fontWeight = "bold";
-}
 
 
