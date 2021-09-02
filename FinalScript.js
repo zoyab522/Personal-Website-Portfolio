2669c5d8c431dbbb55958b4d908b727bf0aec93c
Zoya <zoya.artsy@gmail.com>
2021-09-01 20:21:09 -0400

    fixed bugs

--- FinalScript.js
+++ FinalScript.js
@@ -21,3 +21,14 @@ function showSlides(n, no) {
   }
   x[slideIndex[no]-1].style.display = "block";
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
